42ce0ed3f7910d71f2a55a320e55a9b58e24fc3d
N0usss <anas.baharin1@gmail.com>
2024-09-23 16:37:23 +0800

    updated with scanner

--- MainStudentSystem.java
+++ MainStudentSystem.java
@@ -8,7 +8,7 @@ public class MainStudentSystem {
        StudentSystem student = new StudentSystem(null,0,0,0);
         Scanner scanner = new Scanner(System.in);
 
-        for(int i=0;i<5;i++) {
+        for(int i=0;i<2;i++) {
 
             System.out.print("Name: ");
             String name = scanner.next();
@@ -19,11 +19,15 @@ public class MainStudentSystem {
             System.out.print("Science: ");
             int science = scanner.nextInt();
 
+             student = new StudentSystem(name,math,english,science);
+            System.out.println(student.getName() + " " + student.calculateAverage());
+            student.assignGrade();
 
-            StudentSystem students = new StudentSystem(name,math,english,science);
-            System.out.println(students.getName() + " " + students.calculateAverage());
-            students.assignGrade();
         }
+
+
+
+
 
 
 
