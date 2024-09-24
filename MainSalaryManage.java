c1446859c08803e459bfcff0ed621a3a3651a119
N0usss <anas.baharin1@gmail.com>
2024-09-24 16:49:50 +0800

    updated with try & catch

--- MainSalaryManage.java
+++ MainSalaryManage.java
@@ -1,8 +1,9 @@
 import java.util.*;
-
+import java.sql.*;
 public class MainSalaryManage {
 
     public static void main(String[] args){
+
 
         SalaryManage[] salary = new SalaryManage[5];
 
@@ -10,24 +11,33 @@ public class MainSalaryManage {
 
         String[] employee = {"Abu","Ya","Skibidi","Rizzler","Sigma"};
 
-        for(int i=0; i< employee.length;i++){
+        try {
+            for (int i = 0; i < employee.length; i++) {
 
-            salary[i]= new SalaryManage(employee[i], base[i]);
+                salary[i] = new SalaryManage(employee[i], base[i]);
 
-            salary[i].setHealthAllowance(500.0) ;
-            salary[i].setTransportAllowance(350.0);
+                salary[i].setHealthAllowance(500.0);
+                salary[i].setTransportAllowance(350.0);
 
-            double allowance = salary[i].getHealthAllowance() + salary[i].getTransportAllowance();
+                double allowance = salary[i].getHealthAllowance() + salary[i].getTransportAllowance();
 
 
-            System.out.println("Employee " + i);
-            System.out.println("Name: " + salary[i].getName());
-            System.out.println("Base Salary: " + salary[i].getBaseSalary());
-            System.out.println("Allowance: " + allowance);
-            System.out.println("Bonus: " + salary[i].assignBonus());
-            System.out.println("Total Salary: " + salary[i].calculateTotalSalary());
-            System.out.println("----------------------------------------------------------------------");
+                System.out.println("Employee " + i);
+                System.out.println("Name: " + salary[i].getName());
+                System.out.println("Base Salary: " + salary[i].getBaseSalary());
+                System.out.println("Allowance: " + allowance);
+                System.out.println("Bonus: " + salary[i].assignBonus());
+                System.out.println("Total Salary: " + salary[i].calculateTotalSalary());
+                System.out.println("----------------------------------------------------------------------");
+            }
         }
-
+        catch (Exception e){
+            System.out.println("caught bad " + e.getMessage());
+        }
+        finally {
+            System.out.println("welp continue i guess");
+        }
     }
+
+
 }
